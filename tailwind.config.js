89c35296c3f2530431082e09baafb98f9c8d8db4
davidteren <drteren@gmail.com>
2022-06-15 02:30:04 +0200

    wip: update color themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+// /** @type {import('tailwindcss').Config} */
+
 // const colors = require('tailwindcss/colors')
 // const defaultTheme = require('tailwindcss/defaultTheme')
 
@@ -9,15 +11,28 @@ module.exports = {
     './app/javascript/**/*.js'
   ],
   theme: {
-    extend: {}
-  },
-  corePlugins: {
-    aspectRatio: false
-  },
-  plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/line-clamp')
-  ]
+    extend: {
+      colors: {
+        bg: 'rgb(var(--bg) / <alpha-value>)',
+        'accent-bg': 'rgb(var(--accent-bg) / <alpha-value>)',
+        text: 'rgb(var(--text) / <alpha-value>)',
+        'text-light': 'rgb(var(--text-light) / <alpha-value>)',
+        border: 'rgb(var(--border) / <alpha-value>)',
+        accent: 'rgb(var(--accent) / <alpha-value>)',
+        code: 'rgb(var(--code) / <alpha-value>)',
+        preformatted: 'rgb(var(--preformatted) / <alpha-value>)',
+        marked: 'rgb(var(--marked) / <alpha-value>)',
+        disabled: 'rgb(var(--disabled) / <alpha-value>)',
+      }
+    },
+    corePlugins: {
+      aspectRatio: false
+    },
+    plugins: [
+      require('@tailwindcss/typography'),
+      require('@tailwindcss/forms'),
+      require('@tailwindcss/aspect-ratio'),
+      require('@tailwindcss/line-clamp')
+    ]
+  }
 }
